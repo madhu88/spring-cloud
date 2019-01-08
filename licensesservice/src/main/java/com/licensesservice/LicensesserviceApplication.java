b94cd82acf0f742145ce8f7095e7e7a7e2f7285f
Madhu <madhukar.gp@dxc.com>
2019-01-08 12:02:58 +0530

    added extrated version of licensesservice

---
+++ licensesservice/src/main/java/com/licensesservice/LicensesserviceApplication.java
@@ -0,0 +1,14 @@
+package com.licensesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensesserviceApplication.class, args);
+	}
+
+}
+
