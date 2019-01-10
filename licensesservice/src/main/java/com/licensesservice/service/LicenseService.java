0c1f9cabcd49482f451b2e413e18a29f68fb65e9
Madhu <madhukar.gp@dxc.com>
2019-01-10 14:53:50 +0530

    committing the modifed files for service discovery

--- licensesservice/src/main/java/com/licensesservice/service/LicenseService.java
+++ licensesservice/src/main/java/com/licensesservice/service/LicenseService.java
@@ -4,14 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.licensesservice.clients.OrganizationDiscoveryClient;
+import com.licensesservice.clients.OrganizationFeignClient;
+import com.licensesservice.clients.OrganizationRestTemplate;
 import com.licensesservice.model.License;
 import com.licensesservice.model.Organization;
+import com.licensesservice.model.OrganizationServiceRequest;
 
 @Service
 public class LicenseService {
 	
 	@Autowired
 	private OrganizationDiscoveryClient organizationDiscoveryClient;
+	
+	@Autowired
+	private OrganizationRestTemplate organizationRestTemplate;
+	
+	@Autowired
+	private OrganizationFeignClient organizationFeignClient;
 
 	public License getLicense(String organizationId, String licenseId, String clientType) {
 		License license = new License();
@@ -29,6 +38,22 @@ public class LicenseService {
 				System.out.println("Using dicovery client");
 				organization = organizationDiscoveryClient.getOrganization(organizationId);
 				break;
+				
+			case "rest":
+				System.out.println("Using rest template client");
+				organization = organizationRestTemplate.getOrganization(organizationId);
+				break;
+				
+			case "feign":
+				System.out.println("Using feign client");
+				OrganizationServiceRequest organizationServiceRequest = new OrganizationServiceRequest();
+				organizationServiceRequest.setOrgId(Integer.parseInt(organizationId));
+				organization = organizationFeignClient.getOrganization(organizationServiceRequest);
+				break;
+				
+			default:
+				System.out.println("Using default rest template");
+				organization = organizationRestTemplate.getOrganization(organizationId);
 		}
 		
 		return organization;
