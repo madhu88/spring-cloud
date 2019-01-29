107889816c0d28ec31dbcd91c93cb4b5e6df2135
Madhu <madhukar.gp@dxc.com>
2019-01-29 19:08:13 +0530

    modified for  thread pool properties demo

--- hystrixdemo/src/main/java/com/hystrix/config/HystrixDemoConfig.java
+++ hystrixdemo/src/main/java/com/hystrix/config/HystrixDemoConfig.java
@@ -10,9 +10,22 @@ public class HystrixDemoConfig {
 
 	@Value("${thread.wait.time.ms}")
 	private Long threadWaitTime;
+	
+	@Value("${printThreadPoolDemoPool.coreSize}")
+	private String printThreadPoolDemoPool_coreSize;
+	
+	@Value("${printThreadPoolDemoPool.maxQueueSize}")
+	private String printThreadPoolDemoPool_maxQueueSize;
 
+	public String getPrintThreadPoolDemoPool_coreSize() {
+		return printThreadPoolDemoPool_coreSize;
+	}
+
+	public String getPrintThreadPoolDemoPool_maxQueueSize() {
+		return printThreadPoolDemoPool_maxQueueSize;
+	}
+	
 	public Long getThreadWaitTime() {
 		return threadWaitTime;
-	}
-		
+	}		
 }
--- hystrixdemo/src/main/java/com/hystrix/controller/HystrixDemoController.java
+++ hystrixdemo/src/main/java/com/hystrix/controller/HystrixDemoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hystrix.service.HystrixDemoService;
+import com.hystrix.service.HystrixThreadPoolDemoService;
 
 @RestController
 @RequestMapping(value="/greet")
@@ -16,10 +17,19 @@ public class HystrixDemoController {
 	@Autowired
 	private HystrixDemoService hystrixDemoService;
 	
+	@Autowired
+	private HystrixThreadPoolDemoService hystrixThreadPoolDemoService;
+	
 	@GetMapping(value="/user/{name}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public String greetHello(@PathVariable("name") String name) {
-	System.out.println("Start of greetHello");
+		System.out.println("Start of greetHello");
 		return hystrixDemoService.sayHello(name);
+	}
+	
+	@GetMapping(value="/printinput/{input}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public String printThreadPoolDemo(@PathVariable("input") String input) {
+		System.out.println("Start of printThreadPoolDemo");
+		return hystrixThreadPoolDemoService.printThreadPoolDemo(input);
 	}
 
 }
