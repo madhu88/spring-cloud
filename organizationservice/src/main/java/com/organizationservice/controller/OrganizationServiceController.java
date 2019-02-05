b964e8b6e07134476e5c03410ea6349b9ff1c96e
Madhu <madhukar.gp@dxc.com>
2019-02-05 11:03:58 +0530

    added code to read from config server

--- organizationservice/src/main/java/com/organizationservice/controller/OrganizationServiceController.java
+++ organizationservice/src/main/java/com/organizationservice/controller/OrganizationServiceController.java
@@ -1,5 +1,6 @@
 package com.organizationservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.organizationservice.config.ConfigEntries;
 import com.organizationservice.model.request.OrganizationServiceRequest;
 import com.organizationservice.model.response.OrganizationServiceResponse;
 
@@ -15,9 +17,30 @@ import com.organizationservice.model.response.OrganizationServiceResponse;
 @RequestMapping(value="/organization")
 public class OrganizationServiceController {
 	
+	@Autowired
+	private ConfigEntries configEntries;
+	
 	@PostMapping(value="/getname", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> getOrgName(@RequestBody OrganizationServiceRequest organizationServiceRequest) {
 		System.out.println("organizationServiceRequest.getOrgId = " + organizationServiceRequest.getOrgId());
+		ResponseEntity<Object> responseEntity = null;
+		OrganizationServiceResponse organizationServiceResponse = new OrganizationServiceResponse();
+		organizationServiceResponse.setOrgId(organizationServiceRequest.getOrgId());
+		organizationServiceResponse.setOrgName("Test name for org with ID " +  organizationServiceRequest.getOrgId());
+		responseEntity = new ResponseEntity<>(organizationServiceResponse, HttpStatus.OK);
+		return responseEntity;
+	}
+	
+	@PostMapping(value="/greet", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> greetEmp(@RequestBody OrganizationServiceRequest organizationServiceRequest) {
+		System.out.println("organizationServiceRequest.getOrgId = " + organizationServiceRequest.getOrgId());
+		System.out.println("greetEmp timeOut = " + configEntries.getGreetTimeout());
+		try {
+			Thread.sleep(configEntries.getGreetTimeout());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ResponseEntity<Object> responseEntity = null;
 		OrganizationServiceResponse organizationServiceResponse = new OrganizationServiceResponse();
 		organizationServiceResponse.setOrgId(organizationServiceRequest.getOrgId());
