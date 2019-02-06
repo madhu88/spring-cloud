ed126a9dc6f83726c03c45ad6a571f3311059653
Madhu <madhukar.gp@dxc.com>
2019-02-06 14:22:51 +0530

    added code to register to eureka

--- hystrixdemo/src/main/java/com/hystrix/HystrixdemoApplication.java
+++ hystrixdemo/src/main/java/com/hystrix/HystrixdemoApplication.java
@@ -3,8 +3,10 @@ package com.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class HystrixdemoApplication {
 
