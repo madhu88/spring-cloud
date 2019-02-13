fe8b26c5d1170d5df3012b06ef00a203ff4c08cd
Madhu <madhukar.gp@dxc.com>
2019-02-13 11:49:25 +0530

    added authservice

---
+++ authservice/src/main/java/com/authservice/AuthserviceApplication.java
@@ -0,0 +1,35 @@
+package com.authservice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
+public class AuthserviceApplication {
+	
+	@RequestMapping(value = {"/user"}, produces = "application/json")
+	public Map<String, Object> user(OAuth2Authentication user) {
+		Map<String, Object> userInfo = new HashMap<String, Object>();
+		userInfo.put("user", user.getUserAuthentication().getPrincipal());
+		userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
+		return userInfo;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserviceApplication.class, args);
+	}
+
+}
+
---
+++ authservice/src/main/java/com/authservice/controller/AuthServiceController.java
@@ -0,0 +1,16 @@
+package com.authservice.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class AuthServiceController {
+
+	
+
+}
