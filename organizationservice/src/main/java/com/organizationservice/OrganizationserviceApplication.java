edf5cf01687e61c41ba06029f4301846d8abe6f7
Madhu <madhukar.gp@dxc.com>
2019-02-15 11:30:10 +0530

    modified and added for oauth2 changes

--- organizationservice/src/main/java/com/organizationservice/OrganizationserviceApplication.java
+++ organizationservice/src/main/java/com/organizationservice/OrganizationserviceApplication.java
@@ -2,8 +2,10 @@ package com.organizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class OrganizationserviceApplication {
 
 	public static void main(String[] args) {
